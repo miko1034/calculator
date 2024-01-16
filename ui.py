2b3cc1256425d2f87d08954c7907595e0d7a9d9b
miko1034 <84585119+miko1034@users.noreply.github.com>
2024-01-16 18:42:42 +0000

    ui update and developments

--- ui.py
+++ ui.py
@@ -3,17 +3,34 @@ from tkinter import *
 window = Tk()
 window.geometry("500x500")
 
-startframe = Frame(window)
+def raiseframe(frame):
+    frame.tkraise()
+    
+def getEntry():
+    pass
+
+#creation of all frames
+
+fcalc = Frame(window, background="light blue")
+fcalc.place(x=0,y=0,height=500,width=500)
+startframe = Frame(window, background="light blue")
+startframe.place(x=0,y=0,height=500,width=500)
+
 #startframe objects
-starttext = Label(window, text="Calculator")
-starttext.place(y=100)
-starttext.configure(anchor="center")
+starttext = Label(startframe, text="Calculator",font=("Arial",30), background="light blue")
+starttext.place(x=165,y=20)
+continuebutton = Button(startframe, text="Basic Calc", command=lambda:raiseframe(fcalc),font=("Arial",15))
+continuebutton.place(width=100,x=60,y=110)
+
+#fcalc objects
+fcalcTitle = Label(fcalc, text="Normal Calculations",font=("Arial",25), background="light blue")
+fcalcTitle.place(x=105,y=20)
+fcalcBack = Button(fcalc, text="<--Back", command=lambda:raiseframe(startframe),font=("Arial",10))
+fcalcBack.place(x=10,y=10)
+##actual calculator
+fcalcEntry = Entry(fcalc, text="Enter Math Here")
+fcalcEntry.place(x=115,y=100,width=300,height=200) # not finished here. carry on from here
 
 
-mainframe = Frame(window)
-#packing frames
-startframe.pack()
-startframe.pack_propagate(False)
-startframe.configure(background="light blue", width=500,height=500)
 
 window.mainloop()
