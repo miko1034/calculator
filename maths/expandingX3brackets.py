b7751a9953aa907bb0545371539477a4a724f3aa
miki <84585119+miko1034@users.noreply.github.com>
2024-03-13 09:02:23 +0000

    broken

---
+++ maths/expandingX3brackets.py
@@ -0,0 +1,10 @@
+b1=input("Enter Bracket 1: ")
+b2=input("Enter Bracket 2: ")
+b3=input("Enter Bracket 3: ")
+
+def clean(b1,b2,b3):
+    b1Val = b1[]
+
+vals = clean(b1,b2,b3)
+
+print(vals[0],vals[1],vals[2])
